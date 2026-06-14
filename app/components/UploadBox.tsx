26c71053f6f92f27fe00db2d199a81b976c61c1c
abhi1906singh <singhabhinandan634@gmail.com>
2026-06-14 18:56:36 +0530

    Added the upload page funtionality and design

--- app/components/UploadBox.tsx
+++ app/components/UploadBox.tsx
@@ -1,10 +1,35 @@
+import { Upload, X } from "lucide-react";
+import { useRef, useState } from "react";
+
 export default function UploadBox() {
+    
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const [fileName,setFileName] = useState<string | null>(null);
+    function handleClick() {
+        fileRef?.current?.click()
+    }
+
+    function handleFileChange(e:React.ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        setFileName(file.name)
+    }
+
+    function handleCancel() {
+        setFileName(null);
+    }
     return (
-        <>
-            <div className="flex gap-5">
-                Upload here:
-                <input type="file"  id="file" name="file" className="bg-red-300"/>
-       </div>
-        </>
+        <div className="flex flex-col gap-4">
+        <div className="upload-box border border-gray-400" onClick={handleClick}>
+            <div>This is the upload box</div> 
+            <input type="file" accept=".pdf" className="hidden" ref={fileRef} onChange={handleFileChange}/>
+            <Upload />
+            </div>
+            {fileName &&(
+             <div className="flex gap-3">
+                <div>{fileName }</div>
+                <button onClick={handleCancel}><X/></button>
+                </div>
+            )}
+            </div>
     )
 }
