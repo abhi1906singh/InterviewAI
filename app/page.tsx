508210802bde0554aa9de745aa083e50539c803d
abhi1906singh <singhabhinandan634@gmail.com>
2026-08-11 16:41:53 +0530

    Integrated prisma with Supabase postgresql

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth,currentUser  } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { prisma } from "../app/lib/prisma";
 
 export default async function Home() {
   const { userId } = await auth();
 
-  if (userId) {
-    redirect("/dashboard");
+  if (!userId) {
+    redirect("/sign-in");
   }
+  const user = await currentUser();
 
-  redirect("/sign-in");
+  await prisma.user.upsert({
+    where: {
+      id: userId,
+    },
+    update: {},
+    create: {
+      id: userId,
+      email: user!.emailAddresses[0].emailAddress,
+      firstName: user!.firstName,
+      lastName: user!.lastName,
+      imageUrl: user!.imageUrl,
+    },
+  });
+
+
+  redirect("/dashboard");
 }
